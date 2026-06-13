dc20768b1bfa93fe270874d88799e6a7b1e1893f
Luis Felipe <felipenepocontato@gmail.com>
2026-06-13 17:46:53 -0300

    fix: corrigir concatenação desnecessária em Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Locadora locadora = new Locadora("Kaique Henrique" +
-                "", "Rua das Flores", 100);
+        Locadora locadora = new Locadora("Kaique Henrique", "Rua das Flores", 100);
         Menu menu = new Menu(locadora);
         menu.iniciar();
     }
